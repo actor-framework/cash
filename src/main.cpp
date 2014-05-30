1b97248015009b55919fd419635c7aa2056232fe
Dominik Charousset <dominik.charousset@haw-hamburg.de>
2014-05-30 16:08:01 +0200

    Initial commit

---
+++ src/main.cpp
@@ -0,0 +1,29 @@
+/******************************************************************************\
+ *                                                                            *
+ *   ____         __                  ____    __              ___    ___      *
+ *  / _  \       /\ \__              /\  _`\ /\ \            /\_ \  /\_ \     *
+ * /\ \L\ \    __\ \ ,_\   ___   _ __\ \,\L\_\ \ \___      __\//\ \ \//\ \    *
+ * \ \  __ \  /'__\ \ \/  / __`\/\`'__\/_\__ \\ \  _ `\  /'__`\\ \ \  \ \ \   *
+ *  \ \ \/\ \/\ __/\ \ \_/\ \L\ \ \ \/  /\ \L\ \ \ \ \ \/\  __/ \_\ \_ \_\ \  *
+ *   \ \_\ \_\ \___\\ \__\ \____/\ \_\  \ `\____\ \_\ \_\ \____\/\____\/\___\ *
+ *    \/_/\/_/\/___/ \/__/\/___/  \/_/   \/_____/\/_/\/_/\/____/\/____/\/___/ *
+ *                                                                            *
+ *                                                                            *
+ * Copyright (C) 2011 - 2014                                                  *
+ * Dominik Charousset <dominik.charousset (at) haw-hamburg.de>                *
+ * Alex    Mantel     <alex.mantel        (at) haw-hamburg.de>                *
+ *                                                                            *
+ * Distributed under the Boost Software License, Version 1.0. See             *
+ * accompanying file LICENSE or copy at http://www.boost.org/LICENSE_1_0.txt  *
+\******************************************************************************/
+
+
+#include <iostream>
+
+#include "editline/readline.h"
+
+#include "actorshell/actorshell.hpp"
+
+int main(int argc, char** argv) {
+
+}
