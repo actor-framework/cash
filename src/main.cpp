39ca8ef4d679309e2229018e2054662a1cdbda8e
Alex Mantel <alexmantel93@gmail.com>
2014-07-14 14:00:08 +0000

    Added .gitmodules

--- src/main.cpp
+++ src/main.cpp
@@ -1,63 +1,76 @@
-/******************************************************************************\
- *                                                                            *
- *   ____         __                  ____    __              ___    ___      *
- *  / _  \       /\ \__              /\  _`\ /\ \            /\_ \  /\_ \     *
- * /\ \L\ \    __\ \ ,_\   ___   _ __\ \,\L\_\ \ \___      __\//\ \ \//\ \    *
- * \ \  __ \  /'__\ \ \/  / __`\/\`'__\/_\__ \\ \  _ `\  /'__`\\ \ \  \ \ \   *
- *  \ \ \/\ \/\ __/\ \ \_/\ \L\ \ \ \/  /\ \L\ \ \ \ \ \/\  __/ \_\ \_ \_\ \  *
- *   \ \_\ \_\ \___\\ \__\ \____/\ \_\  \ `\____\ \_\ \_\ \____\/\____\/\___\ *
- *    \/_/\/_/\/___/ \/__/\/___/  \/_/   \/_____/\/_/\/_/\/____/\/____/\/___/ *
- *                                                                            *
- *                                                                            *
- * Copyright (C) 2011 - 2014                                                  *
- * Dominik Charousset <dominik.charousset (at) haw-hamburg.de>                *
- * Alex    Mantel     <alex.mantel        (at) haw-hamburg.de>                *
- *                                                                            *
- * Distributed under the Boost Software License, Version 1.0. See             *
- * accompanying file LICENSE or copy at http://www.boost.org/LICENSE_1_0.txt  *
-\******************************************************************************/
-
-
-
-#include <string>
-#include <memory>
-#include <vector>
 #include <iostream>
 
-#include "actorshell/actorshell.hpp"
+#include "sash/sash.hpp"
+#include "sash/libedit_backend.hpp" // our backend
+#include "sash/variables_engine.hpp"
 
 using namespace std;
-using namespace actorshell;
 
-
-
-
-
-int main(int argc, char** argv) {
-
-    shell        actor_shell(" > ");
-
-    for (;;) {
-
-        std::unique_ptr<char, void (*)(void*)> input {
-            readline(actor_shell.get_prompt().c_str()), free
-        };
-
-
-
-        add_history(input.get());
-        if (!input) break;
-
-        /*
-        if (binary_search(commands.begin, commands.end, string(input.get()))) {
-           // execute_command(input.get());
-            cout << "Size of History: " << history_length << endl;
-        } else {
-            cerr << "Invalid command: " << input.get() << endl;
-            cerr << "Type 'help' for commands" << endl;
+int main()
+{
+  using char_iter = string::const_iterator;
+  using sash::command_result;
+  sash::sash<sash::libedit_backend>::type cli;
+  string line;
+  auto mptr = cli.mode_add("default", "SASH> ");
+  cli.mode_push("default");
+  cli.add_preprocessor(sash::variables_engine<>::create());
+  bool done = false;
+  mptr->add_all({
+    {
+      "quit", "terminates the whole thing",
+      [&](string& err, char_iter first, char_iter last) -> command_result
+      {
+        if (first == last)
+        {
+          done = true;
+          return sash::executed;
         }
-        */
-        input.get();
+        err = "quit: to many arguments (none expected)";
+        return sash::no_command;
+      }
+    },
+    {
+      "echo", "prints its arguments",
+      [](string&, char_iter first, char_iter last) -> command_result
+      {
+        copy(first, last, ostream_iterator<char>(cout));
+        cout << endl;
+        return sash::executed;
+      }
+    },
+    {
+      "help", "prints this text",
+      [&](string& err, char_iter first, char_iter last) -> command_result
+      {
+        if (first == last)
+        {
+          // doin' it complicated!
+          std::string cmd = "echo ";
+          cmd += cli.current_mode().help();
+          return cli.process(cmd);
+        }
+        err = "help: to many arguments (none expected)";
+        return sash::no_command;
+      }
+    }});
+  while (!done)
+  {
+    cli.read_line(line);
+    switch (cli.process(line))
+    {
+    // get connected actors
+      default:
+        break;
+      case sash::nop:
+        break;
+      case sash::executed:
+        cli.append_to_history(line);
+        break;
+      case sash::no_command:
+        cout << cli.last_error()
+             << endl;
+        break;
     }
+  }
 }
-
