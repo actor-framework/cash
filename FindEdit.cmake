837d12681f48d86bdb6de30eac73b2f6462e5684
Jan Christoph Uhde <Jan@UhdeJC.com>
2014-10-14 00:14:57 +0200

    finish rename: editline to edit

--- FindEdit.cmake
+++ FindEdit.cmake
@@ -1,38 +1,38 @@
-# Tries to find editline headers and libraries
+# Tries to find edit headers and libraries
 #
 # Usage of this module as follows:
 #
-#     find_package(Editline)
+#     find_package(Edit)
 #
 # Variables used by this module, they can change the default behaviour and need
 # to be set before calling find_package:
 #
-#  EDITLINE_ROOT_DIR  Set this variable to the root installation of
-#                     editline if the module has problems finding 
+#  EDIT_ROOT_DIR  Set this variable to the root installation of
+#                     edit if the module has problems finding
 #                     the proper installation path.
 #
 # Variables defined by this module:
 #
-#  EDITLINE_FOUND              System has Editline libs/headers
-#  EDITLINE_LIBRARIES          The Editline libraries
-#  EDITLINE_INCLUDE_DIR        The location of Editline headers
+#  EDIT_FOUND              System has Edit libs/headers
+#  EDIT_LIBRARIES          The Edit libraries
+#  EDIT_INCLUDE_DIR        The location of Edit headers
 
-find_path(EDITLINE_INCLUDE_DIR
+find_path(EDIT_INCLUDE_DIR
   NAMES histedit.h
-  HINTS ${EDITLINE_ROOT_DIR}/include)
+  HINTS ${EDIT_ROOT_DIR}/include)
 
-find_library(EDITLINE_LIBRARIES
+find_library(EDIT_LIBRARIES
   NAMES edit
-  HINTS ${EDITLINE_ROOT_DIR}/lib)
+  HINTS ${EDIT_ROOT_DIR}/lib)
 
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(
-  Editline
+  Edit
   DEFAULT_MSG
-  EDITLINE_LIBRARIES
-  EDITLINE_INCLUDE_DIR)
+  EDIT_LIBRARIES
+  EDIT_INCLUDE_DIR)
 
 mark_as_advanced(
-  EDITLINE_ROOT_DIR
-  EDITLINE_LIBRARIES
-  EDITLINE_INCLUDE_DIR)
+  EDIT_ROOT_DIR
+  EDIT_LIBRARIES
+  EDIT_INCLUDE_DIR)
